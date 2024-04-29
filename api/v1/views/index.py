fa7ecde92a36be4abdfccd3adc5bde50f6ca0691
Abdellahsyani <syaniabdos@gmail.com>
2024-04-29 16:35:28 +0100

    add api folder

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index file"""
+
+from api.vi.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", strict_slashes=False)
+def attain_status():
+    """/api/v1/status route"""
+    return jsonify(status='OK')
