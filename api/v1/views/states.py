42eee2a7657ee14c5f5888f7494a7e765ea023a9
Abdellahsyani <syaniabdos@gmail.com>
2024-04-30 14:14:14 +0100

    try to fix error in states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,23 +2,24 @@
 """ handle state actions"""
 
 from api.v1.views import app_views
-from models.state import State
-from models import storage
 from flask import jsonify, abort, request
+from models import storage
+from models.state import State
 
 
-@app_views.route("/states", methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route("/states",
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def states():
     """handle get and post requests"""
     if request.method == 'GET':
-        states = storage.all('states').values()
-        state_list = [state.to_dict() for state in states]
-        return jsonify(state_list)
-    else:
-        post_req = request.get_json(force=True, silent=True)
-        if post_req is None:
+        states = storage.all("State").values()
+        return jsonify(list(map(lambda x: x.to_dict(), states)))
+    else:  # POST
+        kwargs = request.get_json(force=True, silent=True)
+        if kwargs is None:
             abort(400, "Not a JSON")
-        if post_req.get('name') is None:
+        if kwargs.get('name') is None:
             abort(400, "Missing name")
         state = State(**kwargs)
         state.save()
@@ -26,24 +27,25 @@ def states():
 
 
 @app_views.route("/states/<uuid:state_id>",
-                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def state(state_id):
     """handle the get delete put requests with state_id"""
     state = storage.get(State, state_id)
     if state is None:
-        abort(400)
-    if request.method ==  'GET':
+        abort(404)
+    if request.method == 'GET':
         return jsonify(state.to_dict())
     elif request.method == 'DELETE':
         state.delete()
         storage.save()
         return jsonify({})
     else:
-        put_req = request.get_json(force=True, silent=True)
-        if put_req is None:
+        kwargs = request.get_json(force=True, silent=True)
+        if kwargs is None:
             abort(400, "Not a JSON")
-        for key in put_req:
+        for key in kwargs:
             if key not in ('id', 'created_at', 'updated_at'):
-                setattr(state, key, put_req.get(key))
+                setattr(state, key, kwargs.get(key))
         storage.save()
         return jsonify(state.to_dict())
