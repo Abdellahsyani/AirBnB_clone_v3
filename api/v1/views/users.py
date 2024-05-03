3c587a6712a91a2cfdd2e1b69fdf314b4230a2c2
Abdellahsyani <syaniabdos@gmail.com>
2024-05-03 21:27:23 +0100

    done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,70 +1,73 @@
 #!/usr/bin/python3
-"""User"""
+""" View for User objects that handles default API actions """
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, make_response, request
 from models import storage
 from models.user import User
+import hashlib
 
 
-@app_views.route("/users", methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def users():
-    """Get all user objects."""
-    users = []
-    for user in storage.all(User).values():
-        users.append(user.to_dict())
-    return jsonify(users)
+    """ Retrieves the list of all User objects """
+    d_users = storage.all(User)
+    return jsonify([obj.to_dict() for obj in d_users.values()])
 
 
-@app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
-def user(user_id):
-    """Get user by user ID."""
-    user = storage.get(User, user_id)
-    if user:
-        return jsonify(user.to_dict())
-    else:
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def r_user_id(user_id):
+    """
+    file: yml/users_get.yml
+    """
+    user = storage.get("User", user_id)
+    if not user:
         abort(404)
+    return jsonify(user.to_dict())
 
 
-@app_views.route("/users/<user_id>", strict_slashes=False, methods=["DELETE"])
-def delete_user(user_id):
-    """Delete user by user ID."""
-    user = storage.get(User, user_id)
-    if user:
-        storage.delete(user)
-        storage.save()
-        return jsonify({}), 200
-    else:
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_user(user_id):
+    """ Deletes a User object """
+    user = storage.get("User", user_id)
+    if not user:
         abort(404)
+    user.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route("/users", strict_slashes=False, methods=["POST"])
-def posted_users():
-    """Create user."""
-    data = request.get_json(force=True, silent=True)
-    if data is None:
-        return jsonify({"error": "Not a JSON"}), 400
-    if 'email' not in data.keys():
-        return jsonify({"error": "Missing email"}), 400
-    if 'password' not in data.keys():
-        return jsonify({"error": "Missing password"}), 400
-    user = User(email=data["email"], password=data["password"])
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """ Creates a User object """
+    new_user = request.get_json()
+    if not new_user:
+        abort(400, "Not a JSON")
+    if "email" not in new_user:
+        abort(400, "Missing email")
+    if "password" not in new_user:
+        abort(400, "Missing password")
+
+    user = User(**new_user)
     storage.new(user)
     storage.save()
-    return jsonify(user.to_dict()), 201
+    return make_response(jsonify(user.to_dict()), 201)
 
 
-@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
-    """Update user object."""
-    data = request.get_json(force=True, silent=True)
-    user = storage.get(User, user_id)
-    if user is None:
+    """ Updates a User object """
+    user = storage.get("User", user_id)
+    if not user:
         abort(404)
-    if data is None:
-        return jsonify({"error": "Not a JSON"}), 400
-    for key in data.keys():
-        if key not in ["id", "email", "created_at", "updated_at"]:
-            setattr(user, key, data[key])
-    user.save()
+
+    body_request = request.get_json()
+    if not body_request:
+        abort(400, "Not a JSON")
+
+    for k, v in body_request.items():
+        if k not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, k, v)
+
     storage.save()
-    return jsonify(user.to_dict()), 200
+    return make_response(jsonify(user.to_dict()), 200)
