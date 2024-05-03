f9ea6e17d76fa0dea00b9ff3fa80944979cd17f3
ila36IX <jbariali002@gmail.com>
2024-05-02 22:30:29 -0700

    I did nothing!

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Users api
-"""
+"""Users api long and I don't what to do more than that!"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
