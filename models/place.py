1f9a22c87ebf655ead52a483dd6f4533844c6d9b
ila36IX <jbariali002@gmail.com>
2024-05-04 19:59:02 +0100

    Places and amenities relations DONE

--- models/place.py
+++ models/place.py
@@ -11,11 +11,11 @@ if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
+                                            cascade="all, delete-orphan"),
                                  primary_key=True),
                           Column('amenity_id', String(60),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
+                                            cascade="all, delete-orphan"),
                                  primary_key=True))
 
 
